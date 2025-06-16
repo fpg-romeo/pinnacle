b66e7dc3edc36e848b4af563585d25f6ab5d2180
fpg-jep <jeffreydimla@fpgins.com>
2025-06-16 16:35:50 +0800

    fixed attachment link

--- app/controllers/GcashController.php
+++ app/controllers/GcashController.php
@@ -102,7 +102,7 @@ class GcashController
 
             if (move_uploaded_file($file_tmp, uploadFile('gcash', $file_new_name))) {
                 // $list = explode(',', $field['row']);
-                $file_upload = getDocumentRoot() . '/upload/gcash/' . $file_new_name;
+                $file_upload = getSiteUrl() . '/file/gcash/' . $file_new_name;
                 
                 try {
 
